3fc5467362fef97089bbb26f4e92458d96ad7a55
sumit bharti <sumitbharti230@gmail.com>
2024-12-04 15:00:54 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,365 @@
+// // 1.
+// // let age = prompt("enter your age")
+// // if( age>=18){
+// //     console.log("adult")
+// // }else{
+// //     console.log("NOt an adult")
+// // }
+
+// // // 2.
+// // let num = prompt("enter a number")
+// // if(num > 0){
+// //     console.log("positive")
+// // }else{
+// //     console.log("not positive")
+// // }
+
+// // 3.
+// // let num = prompt("enter number")
+// // if (num % 2 ==0){
+// //     console.log("even number")
+// // }else{
+
+// //     console.log("odd number")
+// // }
+
+// // 4.
+// // let alpha = prompt("enter alphabet").toLowerCase()
+// // if(alpha=="a" || alpha=="e" || alpha=="i" || alpha=="o" || alpha=="u"){
+// //     console.log("vowel letter")
+// // }else{
+// //     console.log("consonent letter")
+// // }
+
+
+  
+//   // 5. Check Zero
+//   if (number === 0) {
+//     console.log("Zero");
+//   } else {
+//     console.log("Non-zero");
+//   }
+  
+//   // 6. Multiple of Five
+//   if (number % 5 === 0) {
+//     console.log("Multiple of 5");
+//   } else {
+//     console.log("Not a multiple of 5");
+//   }
+  
+//   // 7. Greater Than Ten
+//   if (number > 10) {
+//     console.log("Greater than 10");
+//   } else {
+//     console.log("10 or less");
+//   }
+  
+//   // 8. Check Equality
+//   if (num1 === num2) {
+//     console.log("Equal");
+//   } else {
+//     console.log("Not equal");
+//   }
+  
+//   // 9. String Length Check
+//   if (str.length > 5) {
+//     console.log("Long string");
+//   } else {
+//     console.log("Short string");
+//   }
+  
+//   // 10. Check Uppercase
+//   if (char === char.toUpperCase()) {
+//     console.log("Uppercase");
+//   } else {
+//     console.log("Lowercase");
+//   }
+  
+//   // 11. Check Weekend
+//   if (day === "Saturday") {
+//     console.log("Weekend");
+//   } else {
+//     console.log("Not weekend");
+//   }
+  
+//   // 12. Age Category
+//   if (age < 12) {
+//     console.log("Child");
+//   } else {
+//     console.log("Not a child");
+//   }
+  
+//   // 13. Temperature Check
+//   if (temp > 30) {
+//     console.log("Hot");
+//   } else {
+//     console.log("Cold");
+//   }
+  
+//   // 14. Number Range
+//   if (number >= 1 && number <= 100) {
+//     console.log("In range");
+//   } else {
+//     console.log("Out of range");
+//   }
+  
+//   // 15. Grade Check
+//   if (score >= 50) {
+//     console.log("Pass");
+//   } else {
+//     console.log("Fail");
+//   }
+  
+//   // 16. Divisibility Check
+//   if (number % 3 === 0) {
+//     console.log("Divisible by 3");
+//   } else {
+//     console.log("Not divisible by 3");
+//   }
+  
+//   // 17. Morning or Afternoon
+//   if (time < 12) {
+//     console.log("Morning");
+//   } else {
+//     console.log("Afternoon");
+//   }
+  
+//   // 18. Teenager Check
+//   if (age >= 13 && age <= 19) {
+//     console.log("Teenager");
+//   } else {
+//     console.log("Not a teenager");
+//   }
+  
+//   // 19. String Content Check
+//   if (str.includes("a")) {
+//     console.log("Contains 'a'");
+//   } else {
+//     console.log("Does not contain 'a'");
+//   }
+  
+//   // 20. Leap Year Check
+//   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//     console.log("Leap year");
+//   } else {
+//     console.log("Not a leap year");
+//   }
+  
+//   // 21. Check Negative
+//   if (number < 0) {
+//     console.log("Negative");
+//   } else {
+//     console.log("Positive or zero");
+//   }
+  
+//   // 22. Password Match
+//   if (password === "1234") {
+//     console.log("Access granted");
+//   } else {
+//     console.log("Access denied");
+//   }
+  
+//   // 23. Greater Number
+//   if (num1 > num2) {
+//     console.log("First is greater");
+//   } else {
+//     console.log("Second is greater or equal");
+//   }
+  
+//   // 24. Prime Check
+//   if (number > 1) {
+//     let isPrime = true;
+//     for (let i = 2; i < number; i++) {
+//       if (number % i === 0) {
+//         isPrime = false;
+//         break;
+//       }
+//     }
+//     console.log(isPrime ? "Prime" : "Not prime");
+//   } else {
+//     console.log("Not prime");
+//   }
+  
+//   // 25. Eligible for Discount
+//   if (age >= 65) {
+//     console.log("Eligible for senior discount");
+//   } else {
+//     console.log("Not eligible");
+//   }
+  
+//   // 26. String Comparison
+//   if (str1 === str2) {
+//     console.log("Match");
+//   } else {
+//     console.log("Do not match");
+//   }
+  
+//   // 27. Multiples of Two
+//   if (number % 2 === 0) {
+//     console.log("Multiple of 2");
+//   } else {
+//     console.log("Not a multiple of 2");
+//   }
+  
+//   // 28. Driving License Check
+//   if (age >= 18) {
+//     console.log("Eligible");
+//   } else {
+//     console.log("Not eligible");
+//   }
+  
+//   // 29. Letter Case Check
+//   if (char === char.toLowerCase()) {
+//     console.log("Lowercase");
+//   } else {
+//     console.log("Uppercase");
+//   }
+  
+//   // 30. Number Check
+//   if (number >= 0 && number < 10) {
+//     console.log("Single-digit");
+//   } else {
+//     console.log("More than one digit");
+//   }
+  
+//   // 31. Divisibility by 10
+//   if (number % 10 === 0) {
+//     console.log("Divisible by 10");
+//   } else {
+//     console.log("Not divisible by 10");
+//   }
+  
+//   // 32. Check Month
+//   if ([1, 3, 5, 7, 8, 10, 12].includes(month)) {
+//     console.log("31 days");
+//   } else {
+//     console.log("Not 31 days");
+//   }
+  
+//   // 33. Password Length Check
+//   if (password.length >= 8) {
+//     console.log("Valid password");
+//   } else {
+//     console.log("Password too short");
+//   }
+  
+//   // 34. Check for Zero
+//   if (number === 0) {
+//     console.log("Zero");
+//   } else {
+//     console.log("Not zero");
+//   }
+  
+//   // 35. Grade A Check
+//   if (score >= 90) {
+//     console.log("Grade A");
+//   } else {
+//     console.log("Less than Grade A");
+//   }
+  
+//   // 36. Empty String Check
+//   if (str === "") {
+//     console.log("Empty");
+//   } else {
+//     console.log("Not empty");
+//   }
+  
+//   // 37. Multiple of 4
+//   if (number % 4 === 0) {
+//     console.log("Multiple of 4");
+//   } else {
+//     console.log("Not a multiple of 4");
+//   }
+  
+//   // 38. Day of the Week
+//   if (day === "Sunday") {
+//     console.log("Sunday");
+//   } else {
+//     console.log("Not Sunday");
+//   }
+  
+//   // 39. String Starts with A
+//   if (str.startsWith("A")) {
+//     console.log("Starts with A");
+//   } else {
+//     console.log("Does not start with A");
+//   }
+  
+//   // 40. Check for Space
+//   if (str.includes(" ")) {
+//     console.log("Contains space");
+//   } else {
+//     console.log("No space");
+//   }
+  
+//   // 41. String Length Even
+//   if (str.length % 2 === 0) {
+//     console.log("Even length");
+//   } else {
+//     console.log("Odd length");
+//   }
+  
+//   // 42. Temperature Conversion
+//   if (temp > 100) {
+//     console.log("Above boiling point");
+//   } else {
+//     console.log("Below boiling point");
+//   }
+  
+//   // 43. Work Hours Check
+//   if (time >= 9 && time <= 17) {
+//     console.log("Working hours");
+//   } else {
+//     console.log("Off hours");
+//   }
+  
+//   // 44. BMI Check
+//   if (bmi >= 18.5 && bmi <= 24.9) {
+//     console.log("Healthy");
+//   } else {
+//     console.log("Not healthy");
+//   }
+  
+//   // 45. Check Odd Number
+//   if (number % 2 !== 0) {
+//     console.log("Odd");
+//   } else {
+//     console.log("Even");
+//   }
+  
+//   // 46. Check for Digit
+//   if (char >= '0' && char <= '9') {
+//     console.log("Digit");
+//   } else {
+//     console.log("Not a digit");
+//   }
+  
+//   // 47. Check for Exclamation
+//   if (str.endsWith("!")) {
+//     console.log("Exclamation mark");
+//   } else {
+//     console.log("No exclamation mark");
+//   }
+  
+//   // 48. Day Check
+//   if (["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].includes(day)) {
+//     console.log("Weekday");
+//   } else {
+//     console.log("Weekend");
+//   }
+  
+  // 49. Check Square Number
+  let number = prompt("enter")
+  if (Number.isInteger(Math.sqrt(number))) {
+    console.log("Perfect square");
+  } else {
+    console.log("Not a perfect square");
+  }
+  
+//   // 50. Temperature Range Check
+//   if (temp >= 15 && temp <= 25) {
+//     console.log("Comfortable");
+//   } else {
+//     console.log("Uncomfortable");
+//   }
+  
